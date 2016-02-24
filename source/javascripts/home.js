fb0a0c028eb59903cff24752a76e5509574fff38
Maxime Braud <mx.braud@gmail.com>
2016-02-25 00:43:00 +0100

    homepage changes

--- source/javascripts/home.js
+++ source/javascripts/home.js
@@ -2,6 +2,12 @@ $(function() {
 
   $("#cantattend").click(function() {
     $("#periscope").toggleClass("hidden");
-  });
+    if($("#cantattend").text() == "Got it!") {
+      $("#cantattend").text("Show me again!");
+    }
+    else {
+      $("#cantattend").text("Got it!");
+    }
+      });
 
 });
