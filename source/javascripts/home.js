4f1a4ad2e8a04f285b85196c8d462682a0180502
Maxime Braud <mx.braud@gmail.com>
2016-02-25 16:17:35 +0100

    social

--- source/javascripts/home.js
+++ source/javascripts/home.js
@@ -8,6 +8,25 @@ $(function() {
     else {
       $("#cantattend").text("Got it!");
     }
-      });
+  });
+
+  $("#snaphover").click(function() {
+    $("#snapcode").toggleClass("hidden");
+    $("#snapcode-bg").toggleClass("hidden");
+  //   $("#staytuned").toggle(
+  //     function(){$(".section-heading").css({"visibility": "hidden"});},
+  //     function(){$("#staytuned").css({"opacity": "0"});
+  //   });
+  });
+
+  $("#snapcode").click(function() {
+    $("#snapcode").toggleClass("hidden");
+    $("#snapcode-bg").toggleClass("hidden");
+  });
+
+  $("#snapcode-bg").click(function() {
+    $("#snapcode").toggleClass("hidden");
+    $("#snapcode-bg").toggleClass("hidden");
+  });
 
 });
